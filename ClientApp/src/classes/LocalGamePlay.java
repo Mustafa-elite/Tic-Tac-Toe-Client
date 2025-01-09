7d9cc7cb1a2d05702e7ff37fd0b076ad32c24f47
moaz <moaazmamdoouh@gmail.com>
2025-01-09 19:04:12 +0200

    added localPlayGame class

---
+++ ClientApp/src/classes/LocalGamePlay.java
@@ -0,0 +1,39 @@
+package classes;
+
+public class LocalGamePlay extends GamePlay {
+
+    String print ;
+    Boolean checkWin  ; 
+
+    String playXO(int position) {
+        //player 1 turn 
+        if (!turn) {
+            print = "x";
+            if (position == 0 || position == 1 || position == 2) {
+                b1.boardxy[0][position] = 0;
+            }
+            if (position == 3 || position == 4 || position == 5) {
+                b1.boardxy[1][position] = 0;
+            }
+            if (position == 6 || position == 7 || position == 8) {
+                b1.boardxy[2][position] = 0;
+            }
+        } //player 2 turn 
+        else {
+                print = "o";
+                if (position == 0 || position == 1 || position == 2) {
+                    b1.boardxy[0][position] = 1;
+                }
+                if (position == 3 || position == 4 || position == 5) {
+                    b1.boardxy[1][position] = 1;
+                }
+                if (position == 6 || position == 7 || position == 8) {
+                    b1.boardxy[2][position] = 1;
+                }
+        }
+        checkWin = checkWinner() ; 
+        if(checkWin) print = "w" ; 
+        return print;
+    }
+
+}
