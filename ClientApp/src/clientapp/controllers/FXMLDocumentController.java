c04a95d217f708358ae424cebf7d80ce77e7134c
moaz <moaaz.mamdoouh@gmail.com>
2025-01-16 09:21:07 +0200

    adding login and registration ui

---
+++ ClientApp/src/clientapp/controllers/FXMLDocumentController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientapp.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author uesr
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private TextField user_name_tf_login;
+    @FXML
+    private PasswordField password_tf_login;
+    @FXML
+    private Label user_name_label;
+    @FXML
+    private Label password_label;
+    @FXML
+    private Button button_login;
+    @FXML
+    private Text creat_account_t_login;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
