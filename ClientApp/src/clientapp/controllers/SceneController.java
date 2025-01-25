c06ff571fec9501008dd1b3824824a91333b3223
Khalid Amr Tammam <khalidtammam062@gmail.com>
2025-01-25 23:30:46 +0200

    Refreshing Problems Solved

--- ClientApp/src/clientapp/controllers/SceneController.java
+++ ClientApp/src/clientapp/controllers/SceneController.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clientapp.controllers;
 
 import java.io.IOException;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -15,89 +9,80 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-/**
- *
- * @author user
- */
 public abstract class SceneController {
+
     private static Stage stage;
     private static Scene scene;
     private static Parent root;
-    public static void navigateToHome(Event event) throws IOException
-    {
+
+    public static void navigateToHome(Event event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/HomePage.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public static void navigateToOnlinePlayers(Event event) throws IOException
-    {
 
-        root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/onlineClientsList.fxml"));
-        if(event!=null)
-        {    
-            stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+    public static void navigateToOnlinePlayers(Event event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(SceneController.class.getResource("/clientapp/views/onlineClientsList.fxml"));
+        root = loader.load();
+        OnlineClientsListController controller = loader.getController();
+        controller.startRefreshing();
+        if (event != null) {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         }
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public static void navigateToXOBoard(Event event) throws IOException
-    {
 
+    public static void navigateToXOBoard(Event event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/Board.fxml"));
-        if(event!=null)
-        {    
-            stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        if (event != null) {
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         }
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public static void navigateToLogin(Event event) throws IOException
-    {
+
+    public static void navigateToLogin(Event event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/Login.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    
-    public static void navigateToSignup(Event event) throws IOException
-    {
+
+    public static void navigateToSignup(Event event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/Registeration.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public static void navigateToGameRecords(Event event) throws IOException
-    {
+
+    public static void navigateToGameRecords(Event event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/PreviousMatches.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-    public static void navigateGameInitializer(Event event) throws IOException
-    {
+
+    public static void navigateGameInitializer(Event event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/GameInitializer.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void navigateToProfile(MouseEvent event) throws IOException {
+    public static void navigateToProfile(MouseEvent event) throws IOException {
         root = FXMLLoader.load(SceneController.class.getResource("/clientapp/views/Profile.fxml"));
-        stage=(Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
-    
-    
 }
